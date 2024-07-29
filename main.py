d079105c5f947ec6b40705b134754c39079a4632
Edwin Rivera <roedwin92@gmail.com>
2024-07-29 12:58:33 -0600

    Permitiendo todos los origenes

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ app = FastAPI()
 # Configurar CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Orígenes permitidos
+    allow_origins=["*"],  # Orígenes permitidos
     allow_credentials=True,
     allow_methods=["*"],  # Métodos permitidos
     allow_headers=["*"],  # Headers permitidos

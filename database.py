d9e7c13bcef479b43902380f24377bc6752479ca
Edwin Rivera <roedwin92@gmail.com>
2024-07-29 11:57:42 -0600

    modificando configurancion de mongoDB por segunda vez

--- database.py
+++ database.py
@@ -1,9 +1,13 @@
+import logging
 from pymongo import MongoClient
 import certifi
 
-#client = MongoClient("mongodb://localhost:27017/")
+logging.basicConfig(level=logging.INFO)
 
 MONGO_DETAILS = "mongodb+srv://gaedwin92:gsQOnWybUUmSoTx1@cluster0.gynwpry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
-client = MongoClient(MONGO_DETAILS, tlsCAFile=certifi.where())
-
-db = client["technical_evaluation"]
+try:
+    client = MongoClient(MONGO_DETAILS, tlsCAFile=certifi.where())
+    db = client["technical_evaluation"]
+    logging.info("Successfully connected to MongoDB")
+except Exception as e:
+    logging.error("Could not connect to MongoDB", exc_info=True)

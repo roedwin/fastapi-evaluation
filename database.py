089c007e16d72e46c3cf4790103eadf56107e283
Edwin Rivera <roedwin92@gmail.com>
2024-07-29 11:49:36 -0600

    modificando configurancion de mongoDB

--- database.py
+++ database.py
@@ -1,5 +1,9 @@
 from pymongo import MongoClient
+import certifi
 
 #client = MongoClient("mongodb://localhost:27017/")
-client = MongoClient("mongodb+srv://gaedwin92:gsQOnWybUUmSoTx1@cluster0.gynwpry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+
+MONGO_DETAILS = "mongodb+srv://gaedwin92:gsQOnWybUUmSoTx1@cluster0.gynwpry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+client = MongoClient(MONGO_DETAILS, tlsCAFile=certifi.where())
+
 db = client["technical_evaluation"]

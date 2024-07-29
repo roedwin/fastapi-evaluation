c1623667391d36216aab8e20c2e640550fb70ed4
Edwin Rivera <roedwin92@gmail.com>
2024-07-29 10:41:07 -0600

    Cambio de la base de datos a Atlas

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+#client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb+srv://gaedwin92:gsQOnWybUUmSoTx1@cluster0.gynwpry.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
 db = client["technical_evaluation"]

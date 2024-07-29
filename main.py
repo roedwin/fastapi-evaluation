850e765c1062ca8ce1a02fc14f4b3a37cd3a73d6
Edwin Rivera <roedwin92@gmail.com>
2024-07-29 11:17:16 -0600

    Modificando los puertos

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from routers import providers, products
@@ -15,3 +16,8 @@ app.add_middleware(
 
 app.include_router(providers.router)
 app.include_router(products.router)
+
+if __name__ == "__main__":
+    import uvicorn
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
